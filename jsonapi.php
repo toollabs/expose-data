21fa2d1c868e58c73aba8286f550cc4ae6bcecaa
Rainer Rillke <rillke@wikipedia.de>
2014-04-08 14:06:14 +0200

    Create jsonapi.php

---
+++ jsonapi.php
@@ -0,0 +1,68 @@
+<?php
+$tool_user_name = 'expose-data';
+
+include_once ( 'shared/common.php' ) ;
+error_reporting( E_ALL & ~E_NOTICE ); # Don't clutter the directory with unhelpful stuff
+
+$prot = getProtocol();
+if ( array_key_exists( 'HTTP_ORIGIN', $_SERVER ) ) {
+	$origin = $_SERVER['HTTP_ORIGIN'];
+}
+
+
+// Response Headers
+header('Content-type: application/json; charset=utf-8');
+header('Cache-Control: private, s-maxage=0, max-age=0, must-revalidate');
+header('x-content-type-options: nosniff');
+header('X-Frame-Options: SAMEORIGIN');
+header('X-JSONAPI-VERSION: 0.0.0.0');
+  
+if ( isset( $origin ) ) {
+	// Check protocol
+	$protOrigin = parse_url( $origin, PHP_URL_SCHEME );
+	if ($protOrigin != $prot) {
+		header('HTTP/1.0 403 Forbidden');
+		if ('https' == $protOrigin) {
+			echo '{"error":"Please use this service over https."}';
+		} else {
+			echo '{"error":"Please use this service over http."}';
+		}
+		exit;
+	}
+	
+	// Do we serve content to this origin?
+	if ( matchOrigin( $origin ) ) {
+		header('Access-Control-Allow-Origin: ' . $origin);
+		header('Access-Control-Allow-Methods: GET');
+	} else {
+		header('HTTP/1.0 403 Forbidden');
+		echo '{"error":"Accesing this tool from the origin you are attempting to connet from is not allowed."}';
+		exit;
+	}
+}
+
+// There are more clever ways to achieve this but for now, it should be sufficient
+$action = '';
+if ( array_key_exists('action', $_REQUEST) ) {
+	$action = $_REQUEST['action'];
+}
+switch ($action) {
+	case 'sha1lookup':
+		include_once ( 'php/sha1lookup.php' );
+		$sha1 = '';
+		
+		if ( array_key_exists('sha1', $_REQUEST) ) {
+			$sha1 = $_REQUEST['sha1'];
+		}
+		$res['sha1lookup'] = sha1lookup( $sha1 );
+		break;
+	default:
+		header('HTTP/1.0 501 Not implemented');
+		$res['error'] = 'Unknown action "' . $action . '". Allowed are sha1lookup, .';
+		break;
+}
+if (!isset( $res )) {
+	$res[] = array();
+}
+echo json_encode($res);
+?>
