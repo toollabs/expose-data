1d2c510b268bf5efad54909a08c06055a6bc00bd
Rainer Rillke <rillke@wikipedia.de>
2014-04-14 20:03:02 +0200

    Create uploadcount.php

---
+++ php/uploadcount.php
@@ -0,0 +1,28 @@
+<?php
+include_once ( 'shared/common.php' ) ;
+
+function uploadcount( $username ) {
+	$res = -1;
+
+	// INPUT VALIDATION
+	$db = openDB('commons', 'commons');
+
+	if ( strlen( $username ) > 255 || preg_match( '/(SELECT.+)|(DROP.+)|(ALTER.+)|(UNION.+)|(INSERT.+)|(\{|\}|;|%)/', $username ) ) {
+		return 'Invalid user name supplied.';
+	}
+	$username = $db->real_escape_string( ucfirst ( $username ) );
+
+
+	$sql = "SELECT count(*) AS count FROM image WHERE img_user_text='$username' ORDER BY img_timestamp DESC;";
+
+	if(!$result = $db->query($sql)) {
+		return 'There was an error running the query.'; // [' . $db->error . ']
+	}
+
+	while($row = $result->fetch_assoc()){
+		$res = $row['count'];
+	}
+
+	return $res;
+}
+?>
