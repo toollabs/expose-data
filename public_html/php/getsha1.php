a9583ca432d97b401e33e28c13f2001d51a1a63c
Steinsplitter <steinsplitter-wiki@live.com>
2019-06-10 10:41:09 +0200

    fix
    ERROR 1054 (42S22): Unknown column 'fa_user_text' in 'field list'

--- public_html/php/getsha1.php
+++ public_html/php/getsha1.php
@@ -9,7 +9,7 @@ function getsha1( $filename ) {
 	$filename = str_replace( ' ', '_', $filename );
 	$filename = $db->real_escape_string( $filename );
 
-	$sql = "SELECT fa_name, fa_size, fa_user_text, fa_description, fa_sha1, fa_timestamp FROM filearchive WHERE fa_name='" . $filename . "';";
+	$sql = "SELECT fa_name, fa_size, actor_name AS fa_user_text, comment_text AS fa_description, fa_sha1, fa_timestamp FROM filearchive INNER JOIN actor ON fa_actor = actor_id INNER JOIN comment ON fa_description_id = comment_id WHERE fa_name='" . $filename . "';";
 
 	if(!$result = $db->query($sql)) {
 		return 'There was an error running the query.'; // [' . $db->error . ']
