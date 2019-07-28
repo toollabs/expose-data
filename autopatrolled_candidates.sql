ef0fc3e0e368c7d862f2bab42a4817a1ea09a6b6
Rainer Rillke <rillke@wikipedia.de>
2019-07-28 12:08:31 +0000

    Update Query to obtain autopatrolled candidates
    1. Use action tables
    2. Use comment tables
    3. Can't write temporary tables to Commons DB anymore
    4. See https://wikitech.wikimedia.org/wiki/Talk:Wiki_replicas

--- autopatrolled_candidates.sql
+++ autopatrolled_candidates.sql
@@ -1,41 +1,88 @@
-DROP DATABASE IF EXISTS `s51857__c_tmp`;
-CREATE DATABASE `s51857__c_tmp`;
-CREATE TABLE `s51857__c_tmp`.`tmp_apc_com` ENGINE=MEMORY (
-	SELECT REPLACE(`user_name`, ' ', '_') AS 'uname',
-		`user_id`,
-		`user_name`,
-		`user_editcount`,
-		`user_registration`
-	FROM `user`
-		/* there are very few users with > 1000 edits compared to the overall number of users */
-		WHERE `user`.`user_editcount` > 1000
-		/* DB should be prepared for joining these two (MediaWiki does this all the time); */
-		/* user_groups is supposedly a relatively small table */
-		AND `user`.`user_id` NOT IN ( SELECT `ug_user` FROM `user_groups` )
-		/* do not include brand new users who just experimented with Cat-A-Lot */
-		AND (DATEDIFF( NOW(), `user`.`user_registration` ) > 30 OR `user`.`user_registration` IS NULL)
-		/* and of course these users should not be blocked currently */
-		AND `user`.`user_id` NOT IN ( SELECT `ipb_user` FROM `ipblocks` )
-		/* and have an edit or other contribution within the last 30 days */
-		/* this makes the query notably slower but should be almost constant */
-		/* even if we get a lot new users or operate this wiki another 10 years */
-		/* while user_daily_contribs will grow over time and might consume even more */
-		/* resources in the future */
-		AND `user`.`user_id` IN ( SELECT `rc_user` FROM `recentchanges_userindex` ));
-
-SELECT `user_id`,
-	CONVERT(`user_name` USING 'utf8') AS 'name',
-	CONVERT(`block_reasons` USING 'utf8') AS 'reason',
-	`user_editcount` AS 'editcount',
-	DATE_FORMAT(`user_registration`, '%Y-%m-%d') AS 'regdate'
-FROM `s51857__c_tmp`.`tmp_apc_com`
-LEFT JOIN (SELECT GROUP_CONCAT(`log_comment` SEPARATOR ' // ') AS 'block_reasons', `log_title`
-	FROM `logging_userindex`
-	WHERE `log_type`='block' AND `log_title` IN
-		(SELECT `uname` FROM `s51857__c_tmp`.`tmp_apc_com`)
-		GROUP BY `log_title`)
-	AS `user_selection`
-ON `user_selection`.`log_title`=`s51857__c_tmp`.`tmp_apc_com`.`uname`
-ORDER BY `user_editcount` DESC;
-DROP DATABASE IF EXISTS `s51857__c_tmp`;
+SELECT
+  `user_id`,
+  CONVERT(`user_name` USING 'utf8') AS 'name',
+  CONVERT(`block_reasons` USING 'utf8') AS 'reason',
+  `user_editcount` AS 'editcount',
+  DATE_FORMAT(`user_registration`, '%Y-%m-%d') AS 'regdate'
+FROM
+  (
+    SELECT
+      REPLACE(`user_name`, ' ', '_') AS 'uname',
+      `user_id`,
+      `user_name`,
+      `user_editcount`,
+      `user_registration`
+    FROM
+      `user`
+    WHERE
+      `user`.`user_editcount` > 1000
+      AND `user`.`user_id` NOT IN(
+        SELECT
+          `ug_user`
+        FROM
+          `user_groups`
+      )
+      AND (
+        DATEDIFF(NOW(), `user`.`user_registration`) > 30
+        OR `user`.`user_registration` IS NULL
+      )
+      AND `user`.`user_id` NOT IN(
+        SELECT
+          `ipb_user`
+        FROM
+          `ipblocks`
+      )
+      AND `user`.`user_id` IN(
+        SELECT
+          `actor_user`
+        FROM
+          `recentchanges_userindex`
+          JOIN actor_recentchanges ON rc_actor = actor_id
+      )
+  ) AS q1
+  LEFT JOIN (
+    SELECT
+      GROUP_CONCAT(`comment_text` SEPARATOR ' // ') AS 'block_reasons',
+      `log_title`
+    FROM
+      `logging_logindex`
+      JOIN `comment_logging` ON `log_comment_id` = `comment_id`
+    WHERE
+      `log_type` = 'block'
+      AND `log_title` IN (
+        SELECT
+          REPLACE (`user_name`, ' ', '_') AS 'uname'
+        FROM
+          `user`
+        WHERE
+          `user`.`user_editcount` > 1000
+          AND `user`.`user_id` NOT IN(
+            SELECT
+              `ug_user`
+            FROM
+              `user_groups`
+          )
+          AND (
+            DATEDIFF(NOW(), `user`.`user_registration`) > 30
+            OR `user`.`user_registration` IS NULL
+          )
+          AND `user`.`user_id` NOT IN(
+            SELECT
+              `ipb_user`
+            FROM
+              `ipblocks`
+          )
+          AND `user`.`user_id` IN(
+            SELECT
+              `actor_user`
+            FROM
+              `recentchanges_userindex`
+              JOIN actor_recentchanges ON rc_actor = actor_id
+          )
+      )
+    GROUP BY
+      `log_title`
+  ) AS `user_selection` ON `user_selection`.`log_title` = `q1`.`uname`
+ORDER BY
+  `user_editcount` DESC;
 
--- sql_autopatrolled.sh
+++ sql_autopatrolled.sh
@@ -3,5 +3,5 @@
 export LANG=en_US.UTF-8
 export PATH=/usr/local/bin:/usr/bin:/bin:$PATH
 mysql --defaults-file=~/replica.my.cnf -h commonswiki.labsdb commonswiki_p < ~/autopatrolled_candidates.sql > ~/www/static/autopatrolled_candidates.tsv
-mysql --defaults-file=~/replica.my.cnf -h specieswiki.labsdb specieswiki_p < ~/autopatrolled_candidates.species.sql > ~/www/static/autopatrolled_candidates_species.tsv
+#mysql --defaults-file=~/replica.my.cnf -h specieswiki.labsdb specieswiki_p < ~/autopatrolled_candidates.species.sql > ~/www/static/autopatrolled_candidates_species.tsv
 
